288c31236ed67bd94d90ca85b045ac1d649d23c7
lulrai <neupanenischal1@gmail.com>
2022-12-22 11:42:00 +0100

    added code to upload/retrieve the model from hugging face

--- create_env.sh
+++ create_env.sh
@@ -7,18 +7,20 @@ conda create --quiet --yes -n apfir python="3.8.13"
 echo "Creating environment done."
 echo
 echo "Installing required conda packages.."
-conda install -n apfir ipykernel --yes
-conda install -n apfir nb_conda_kernels --yes
-conda install -n apfir h5py pandas tqdm Pillow scikit-image scikit-learn scipy tqdm --yes
+#conda install -n apfir ipykernel --yes
+#conda install -n apfir nb_conda_kernels --yes
+#conda install -n apfir pytorch-gpu pytorch-lightning ==yes
+conda install -n apfir ipykernel nb_conda_kernels pytorch-gpu pytorch-lightning h5py pandas nbformat tqdm Pillow scikit-image scikit-learn scipy tqdm timm ipywidgets torchinfo --yes
 conda update -n base -c defaults conda --yes
 conda update --all --yes
 echo "Installing required conda packages done."
-echo
-echo "Installing pip packages.."
-CONDA_PATH=$(conda info | grep -i 'base environment' | awk '{print $4}')
-source $CONDA_PATH/etc/profile.d/conda.sh
-conda activate apfir
-pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
-python3 -m ipykernel install --user --name=apfir
-conda deactivate
-echo "Installing pip packages done."
+#echo
+#echo "Installing pip packages.."
+#CONDA_PATH=$(conda info | grep -i 'base environment' | awk '{print $4}')
+#source $CONDA_PATH/etc/profile.d/conda.sh
+#conda activate apfir
+#pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
+#pip install ipywidgets timm
+#python3 -m ipykernel install --user --name=apfir
+#conda deactivate
+#echo "Installing pip packages done."
